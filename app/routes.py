6b58c217f0b4f1d9dd07d9905b378c6332b2a1a1
ducoquelicot <fmeijer@stanford.edu>
2019-08-02 20:40:55 +0200

    finished POST request

--- app/routes.py
+++ app/routes.py
@@ -2,34 +2,30 @@ from app import datasets, db
 from app.models import Dataset, DatasetSchema
 from flask import request, jsonify
 from str2bool import str2bool
+import cgitb, cgi
 
 data_schema = DatasetSchema()
 datas_schema = DatasetSchema(many=True)
 
 @datasets.route('/dataset', methods=['POST'])
 def create_dataset():
-    response = request.get_data()
+    name = request.form['name']
+    area = request.form['area']
+    source = request.form['source']
+    file_type = request.form['file_type']
+    link = request.form['link']
+    dictionary = str2bool(request.form['dictionary'])
+    date_obtained = request.form['date_obtained']
+    clean = str2bool(request.form['clean'])
+    tags = request.form['tags']
+    file = request.files['file']
 
-    # if response != {}:
-    #     name = response['name']
-    #     area = response['area']
-    #     source = response['source']
-    #     file_type = response['file_type']
-    #     link = response['link']
-    #     dictionary = str2bool(response['dictionary'])
-    #     date_obtained = response['date_obtained']
-    #     clean = str2bool(response['clean'])
-    #     tags = response['tags']
-    #     file = response['file'].encode('utf-8')
+    new_dataset = Dataset(name=name, area=area, source=source, file_type=file_type, link=link, dictionary=dictionary,
+        date_obtained=date_obtained, clean=clean, tags=tags, file=file.read())
 
-    # new_dataset = Dataset(name=name, area=area, source=source, file_type=file_type, link=link, dictionary=dictionary,
-    #     date_obtained=date_obtained, clean=clean, tags=tags, file=file)
+    db.session.add(new_dataset)
+    db.session.commit()
 
-    # db.session.add(new_dataset)
-    # db.session.commit()
-
-    # return data_schema.jsonify(new_dataset), 200
-    # else:
-    return response
+    return "New dataset added. The uploaded file was {}".format(file.filename), 200
 
 
