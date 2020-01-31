b400ec2e18f911f4c45d0da7d7ac5605023921ab
ducoquelicot <fmeijer@stanford.edu>
2020-01-31 15:14:44 +0100

    changes 31/1

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 -k gevent --worker-connections 1000 --timeout 120 --workers 4 --access-logfile - --error-logfile - server:datasets
+exec gunicorn -b :5000 -k gevent --worker-connections 1000 --timeout 300 --workers 4 --access-logfile - --error-logfile - server:datasets
